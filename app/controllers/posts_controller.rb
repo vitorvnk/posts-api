d649beb1625a8d4d8471dfd46aa6046188729b70
tarocoleo <leonardo_taroco@hotmail.com>
2023-01-08 16:29:01 -0300

    add update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @post = Post.find params[:id]
+
+    if @post.update post_params
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
