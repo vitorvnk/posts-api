043168473d63cdf694e9b954b1a9b8aa1a420f8a
tarocoleo <leonardo_taroco@hotmail.com>
2023-01-08 16:36:37 -0300

    add destroy method to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find params[:id]
+    @post.destroy
+
+    head :ok
+  end
+
   private
 
   def post_params
